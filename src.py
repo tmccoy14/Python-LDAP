716a9d5ad73cec0d5a9b1707a7a569e8de6dc8a5
Tucker McCoy <tuckermmccoy@gmail.com>
2020-06-15 16:34:27 -0400

    Update src.py

--- src.py
+++ src.py
@@ -1,38 +1,47 @@
 """Third party modules"""
 import ldap
 
+# LDAP settings
+LDAP_SERVER = "ldap://server"
+BASE_DN = "dc=example,dc=com"
+LDAP_LOGIN = "AFSDIGITAL\Admin"
+LDAP_PASSWORD = "password"
+
+# Active Directory search filters
+OBJECT_TO_SEARCH = "(&(objectclass=person)(CN=User))"
+ATTRIBUTES_TO_SEARCH = ["memberOf"]
+
 
 def ldap_connection():
     """open connection with LDAP server"""
 
-    # disabling certificate check for testing purposes
-    ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+    # Open a connection with the LDAP server
+    connect = ldap.initialize(LDAP_SERVER)
 
-    # open a connection with the LDAP server
-    connect = ldap.initialize("ldaps://server")
-
-    # tell server not to chase referrals
+    # Tell LDAP server not to chase referrals
     connect.set_option(ldap.OPT_REFERRALS, 0)
 
     # bind server with credentials
-    connect.simple_bind_s("user", "password")
+    connect.simple_bind_s(LDAP_LOGIN, LDAP_PASSWORD)
 
-    # return ldap connection
+    # Return LDAP connection
     return connect
 
 
 def ldap_search(ldap_connect):
     """search LDAP server"""
+
+    # Search Active Directory
     result = ldap_connect.search_s(
-        "dc=somedomain,dc=com",
-        ldap.SCOPE_SUBTREE,
-        "userPrincipalName=user@somedomain.com",
-        ["memberOf"],
+        BASE_DN, ldap.SCOPE_SUBTREE, OBJECT_TO_SEARCH, ATTRIBUTES_TO_SEARCH
     )
+
+    # Print search results
+    print(result)
 
 
 if __name__ == "__main__":
-    """set up LDAP server connection and execute search"""
+    """Set up LDAP server connection and execute search"""
 
     ldap_connect = ldap_connection()
     ldap_search(ldap_connect)
