74241a4bb32398b4839800314c1650add875b3a9
Tucker McCoy <tuckermmccoy@gmail.com>
2020-06-17 11:47:28 -0400

    Update src.py

--- src.py
+++ src.py
@@ -1,15 +1,20 @@
+"""Standard library"""
+import re
+
 """Third party modules"""
 import ldap
+from ldap import filter as ldap_filter
 
 
 # LDAP settings
 LDAP_SERVER = "ldap://server"
 BASE_DN = "dc=example,dc=com"
-LDAP_LOGIN = "AFSDIGITAL\Admin"
+LDAP_LOGIN = "EXAMPLE\Admin"
 LDAP_PASSWORD = "password"
 
 # Active Directory search filters
-OBJECT_TO_SEARCH = "(&(objectclass=person)(CN=User))"
+USER = ""
+OBJECT_TO_SEARCH = "(&(objectclass=person)(CN=%s))"
 ATTRIBUTES_TO_SEARCH = ["memberOf"]
 
 
@@ -29,20 +34,27 @@ def ldap_connection():
     return connect
 
 
-def ldap_search(ldap_connect):
+def ldap_search(ldap_connect, user):
     """search LDAP server"""
 
-    # Search Active Directory
-    result = ldap_connect.search_s(
-        BASE_DN, ldap.SCOPE_SUBTREE, OBJECT_TO_SEARCH, ATTRIBUTES_TO_SEARCH
+    records = ldap_connect.search_s(
+        BASE_DN,
+        ldap.SCOPE_SUBTREE,
+        ldap_filter.filter_format(OBJECT_TO_SEARCH, (user,)),
+        ATTRIBUTES_TO_SEARCH,
     )
 
-    # Print search results
-    print(result)
+    ldap_connect.unbind_s()
+
+    if "memberOf" in records[0][1]:
+        groups = records[0][1]["memberOf"]
+        result = [re.findall(b"(?:cn=|CN=)(.*?),", group)[0] for group in groups]
+        result = [r.decode("utf-8") for r in result]
+        print(result)
 
 
 if __name__ == "__main__":
     """Set up LDAP server connection and execute search"""
 
     ldap_connect = ldap_connection()
-    ldap_search(ldap_connect)
+    ldap_search(ldap_connect, USER)
